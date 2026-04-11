2926068cfb03346fc49283b41d3bcd8dbd03a8a7
sreejithm26 <sreejithm2612@gmail.com>
2026-04-11 06:02:16 +0530

    Fix: Minor bugs

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
 import uvicorn
 import os
 
-if __name__ == "__main__":
+def main() -> None:
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("founder_gym.api.server:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("founder_gym.api.server:app", host="0.0.0.0", port=port, reload=False)
+
+if __name__ == "__main__":
+    main()
